740b72c462148dc25b803228283b4c36942ec8dc
Ksavir <kevinsavir20@gmail.com>
2022-07-08 01:30:34 +0000

    actualización componentes de react landing page

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,67 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export function Card() {
+
+export function Card(props) {
     return (
-            <div className="card mx-4" style={{"width": "18rem"}}>
-                <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/02/hipertextual-escenas-iron-man-y-avengers-endgame-que-significan-algo-muy-personal-robert-downey-jr-2020166600.jpg?w=1500&quality=50&strip=all&ssl=1" className="card-img-top" alt="..." />
+            <div className="card mx-4 p-0" style={{"width": "32rem" , height: "40rem"}}>
+                <img src={props.imagen} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Tony Stark - IRON MAN</h5>
-                    <p className="card-text">Tony Stark es un genio inventor y multimillonario industrial, que se enfunda en su armadura de tecnología punta para convertirse en el superhéroe Iron Man</p>
-                    <a href="https://marvel.fandom.com/es/wiki/Anthony_Stark_(Tierra-616)" className="btn btn-primary">Meet Iron Man</a>
+                    <h5 className="card-title">{props.titulo}</h5>
+                    <p className="card-text">{props.descripcion}</p>
+                    <a href={props.url} className="btn btn-danger">{props.boton}</a>
                 </div>
             </div>);
 }
 
-export function Card2() {
-  return (
-          <div className="card mx-4" style={{"width": "18rem"}}>
-              <img src="https://www.cinemascomics.com/wp-content/uploads/2020/07/chris-evans-capitan-america-peliculas-de-marvel-960x720.jpg.webp" className="card-img-top" alt="..." />
-              <div className="card-body">
-                  <h5 className="card-title">Steve Rogers - CAPTAIN AMERICA</h5>
-                  <p className="card-text">Steven "Steve" Rogers también conocido como Captain America, el primer vengador, fue rechazado por el Ejército de los Estados Unidos durante la Segunda Guerra Mundial debido a numerosos problemas de salud. Finalmente se ofreció como voluntario para el Proyecto Renacimiento donde recibió el Suero del Súper Soldado desarrollado por Dr. Abraham Erskine. </p>
-                  <a href="https://marvel.fandom.com/es/wiki/Steven_Rogers_(Tierra-616)" className="btn btn-primary">Meet Captain America</a>
-              </div>
-          </div>);
-}
-
-export function Card3() {
-  return (
-          <div className="card mx-4" style={{"width": "18rem"}}>
-              <img src="https://i0.wp.com/geekzpot.net/wp-content/uploads/2020/06/ThorDarkWorld_2194942100-TDW0NNG1._V362444527_SX1080_.jpg?w=1080&ssl=1" className="card-img-top" alt="..." />
-              <div className="card-body">
-                  <h5 className="card-title">THOR ODINSON</h5>
-                  <p className="card-text">Thor es el Dios del Trueno, miembro de la raza conocida como los Asgardianos, un grupo de seres humanoides de la Dimensión de Bolsillo de Asgard, el Reino Eterno. Thor es el hijo de Odin, el Padre de Todo y rey de Asgard.</p>
-                  <a href="https://marvel.fandom.com/es/wiki/Thor_Odinson_(Tierra-616)" className="btn btn-primary">Meet Thor</a>
-              </div>
-          </div>);
-}
-
-export function Card4() {
-  return (
-          <div className="card mx-4" style={{"width": "20rem"}}>
-              <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/08/spider-man1.jpg?resize=1200%2C675&quality=50&strip=all&ssl=1" className="card-img-top" alt="..." />
-              <div className="card-body">
-                  <h5 className="card-title">Peter Park-SPIDER-MAN </h5>
-                  <p className="card-text">Peter Parker es hijo de Richard y Mary Parker, agentes de la CIA y posteriormente de S.H.I.E.L.D. Durante una visita que el brillante estudiante hacía a la Facultad de Ciencias Exactas y Naturales de la UBA, fue picado por una araña que había sido expuesta a los experimentos radiactivos que allí se llevaban a cabo</p>
-                  <a href="https://marvel.fandom.com/es/wiki/Peter_Parker_(Tierra-616)" className="btn btn-primary">Meet Spider-Man</a>
-              </div>
-          </div>);
-}
-
 export function BarraNav() {
     return (
-      <div class="container">
+      <div className="">
   <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Marvel World</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
+  <div className="d-flex justify-content-between w-100" >
+  <a class="navbar-brand" href="#">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/800px-Marvel_Logo.svg.png" href="https://www.marvel.com/" alt="" width="50" height="24" className="d-inline-block align-text m-2"/>
+      Mundo Marvel
+    </a>
+    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup" style={{color:'white'}}>
+      <div className="navbar-nav ">
+        <a className="nav-link active" aria-current="page" href="#">Super Heroes</a>
+        <a className="nav-link" href="#">Villanos</a>
+        <a className="nav-link" href="#">MCU</a>
         <a className="nav-link"></a>
       </div>
     </div>
@@ -73,11 +39,11 @@ export function BarraNav() {
 
 export function Jumbotron() {
     return <div className="jumbotron">
-  <h1 className="display-4">WELCOME TO MARVEL WORLD</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 className="display-5">BIENVENIDO A MUNDO MARVEL</h1>
+  <p className="lead"></p>
   <hr className="my-4"/>
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a className="btn btn-primary btn-lg" href="https://marvel.fandom.com/es/wiki/P%C3%A1gina_principal#:~:text=Marvel%20Wiki%20%7C%20Fandom&text=Marvel%20Wiki%20es%20la%20enciclopedia,Am%C3%A9rica%2C%20Wolverine%20y%20muchos%20m%C3%A1s." role="button">Meet your favorite hero</a>
+  <h4 className="my-4">CONOCERAS LO FASCINANTE DE LOS MEJORES SUPER HEROES DEL INCREIBLE MUNDO MARVEL</h4>
+  <a className="btn btn-danger btn-lg" href="https://marvel.fandom.com/es/wiki/P%C3%A1gina_principal#:~:text=Marvel%20Wiki%20%7C%20Fandom&text=Marvel%20Wiki%20es%20la%20enciclopedia,Am%C3%A9rica%2C%20Wolverine%20y%20muchos%20m%C3%A1s." role="button">Conoce a tu superheroe favorito</a>
 </div>
 }
 
@@ -94,5 +60,12 @@ export function Footer() {
  );
 }
 
+Card.propTypes = {
+  titulo : PropTypes.string,
+  imagen : PropTypes.string,
+  descripcion : PropTypes.string,
+  url : PropTypes.string,
+  boton: PropTypes.string,
 
+}
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Card2, Card3, Card4, BarraNav, Jumbotron, Footer} from "./card.jsx"
+import {Card, BarraNav, Jumbotron, Footer} from "./card.jsx"
 
 //include images into your bundlebrand
 
@@ -8,14 +8,35 @@ import {Card, Card2, Card3, Card4, BarraNav, Jumbotron, Footer} from "./card.jsx
 const Home = () => {
 	return (
 		<div className="" style={{backgroundColor:'black'}}>
-			<div className ="mb-3 d-flex float-right"><BarraNav/></div>
+			<div className ="mb-3 "><BarraNav/></div>
 				<div className ="d-flex text-center justify-content-center " style={{color:'white'}}><Jumbotron/></div>
-		<div className=" text-center row d-flex justify-content-between mt-5 mb-5">
-		<Card/>
-		<Card2/>
-		<Card3/>
-		<Card4/>
+					<div className="text-center row d-flex justify-content-between mt-5 mb-5" style={{height: '100vh'}}>
+						<Card titulo="Tony Stark - IRON MAN" 
+						imagen="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/02/hipertextual-escenas-iron-man-y-avengers-endgame-que-significan-algo-muy-personal-robert-downey-jr-2020166600.jpg?w=1500&quality=50&strip=all&ssl=1"
+						descripcion="Tony Stark es un genio inventor y multimillonario industrial, que se enfunda en su armadura de tecnología punta para convertirse en el superhéroe Iron Man"
+						url ="https://marvel.fandom.com/es/wiki/Anthony_Stark_(Tierra-616)"
+						boton="Conoce a Iron Man" />
+						
+						<Card titulo="Steve Rogers - CAPTAIN AMERICA"
+						imagen="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2019/05/hipertextual-avengers-endgame-futuro-capitan-america-2019781893-scaled.jpg?w=1560&quality=50&strip=all&ssl=1"
+						descripcion="Steven 'Steve' Rogers también conocido como Captain America, el primer vengador, fue rechazado por el Ejército de los Estados Unidos durante la Segunda Guerra Mundial debido a numerosos problemas de salud. Finalmente se ofreció como voluntario para el Proyecto Renacimiento donde recibió el Suero del Súper Soldado desarrollado por Dr. Abraham Erskine."
+						url="https://marvel.fandom.com/es/wiki/Steven_Rogers_(Tierra-616)"
+						boton="Conoce a Captain America" />
+						
+						<Card
+						titulo="THOR ODINSON"
+						imagen="https://i0.wp.com/geekzpot.net/wp-content/uploads/2020/06/ThorDarkWorld_2194942100-TDW0NNG1._V362444527_SX1080_.jpg?w=1080&ssl=1"
+						descripcion="Thor es el Dios del Trueno, miembro de la raza conocida como los Asgardianos, un grupo de seres humanoides de la Dimensión de Bolsillo de Asgard, el Reino Eterno. Thor es el hijo de Odin, el Padre de Todo y rey de Asgard."
+						url="https://marvel.fandom.com/es/wiki/Thor_Odinson_(Tierra-616)"
+						boton="Conoce a Thor" />
+						
+						<Card titulo="Peter Park-SPIDER-MAN"
+						imagen="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/08/spider-man1.jpg?resize=1200%2C675&quality=50&strip=all&ssl=1"
+						descripcion="Peter Parker es hijo de Richard y Mary Parker, agentes de la CIA y posteriormente de S.H.I.E.L.D. Durante una visita que el brillante estudiante hacía a la Facultad de Ciencias Exactas y Naturales de la UBA, fue picado por una araña que había sido expuesta a los experimentos radiactivos que allí se llevaban a cabo"
+						url="https://marvel.fandom.com/es/wiki/Peter_Parker_(Tierra-616)"
+						boton="Conoce a Spider-Man" />
 		</div>
+		
 		<div className="text-center"><Footer/></div>
 		
 		</div>
